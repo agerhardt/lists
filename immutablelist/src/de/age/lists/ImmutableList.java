496ef1fbb3f3b7d726e6ff13387ea7fe18f57783
fumbletweed <urban.tumbleweed@gmail.com>
2012-07-30 23:10:10 +0200

    initial commit added basic operations i want to support

---
+++ immutablelist/src/de/age/lists/ImmutableList.java
@@ -0,0 +1,33 @@
+package de.age.lists;
+
+import java.util.Iterator;
+
+public class ImmutableList<E> implements Iterable<E> {
+
+	public ImmutableList<E> range(int start, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ImmutableList<E> append(E newElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ImmutableList<E> append(ImmutableList<E> newElements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public int size() {
+		// TODO Auto-generated method stub
+				return 0;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
