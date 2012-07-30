13d4d674d45885dc999e1620e79ec5ceb1076c4f
fumbletweed <urban.tumbleweed@gmail.com>
2012-07-30 23:13:57 +0200

    added constructor stubs

--- immutablelist/src/de/age/lists/ImmutableList.java
+++ immutablelist/src/de/age/lists/ImmutableList.java
@@ -1,9 +1,19 @@
 package de.age.lists;
 
 import java.util.Iterator;
+import java.util.List;
 
 public class ImmutableList<E> implements Iterable<E> {
 
+	public ImmutableList(List<E> elements) {
+		// TODO
+	}
+	
+	@SafeVarargs
+	public ImmutableList(E ... elements) {
+		// TODO
+	}
+	
 	public ImmutableList<E> range(int start, int end) {
 		// TODO Auto-generated method stub
 		return null;
