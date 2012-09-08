5416c11ef325ab87a6492c0ca215c02f39ba51ee
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2012-09-08 16:52:25 +0200

    added new testcase to suite

--- immutablelist/test/de/age/lists/AllTests.java
+++ immutablelist/test/de/age/lists/AllTests.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.age.lists.impl.RangeIteratorTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ ImmutableListConstructorTest.class, ImmutableListBaseFunctionalityTest.class, ImmutableListNewListCreationTest.class})
+@SuiteClasses({ ImmutableListConstructorTest.class, ImmutableListBaseFunctionalityTest.class, ImmutableListNewListCreationTest.class, RangeIteratorTest.class })
 public class AllTests {
 
 }
